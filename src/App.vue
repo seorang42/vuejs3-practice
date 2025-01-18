de5832e8ff7fc7e433664e363176e8d4b49c6772
seorang42 <seorang42@naver.com>
2025-01-18 23:49:50 +0900

    2-3 컴포넌트 이해하기

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <app-header></app-header>
+  <app-nav></app-nav>
+  <app-view></app-view>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <h2>nav</h2>
+    <ul>
+      <li>메뉴1</li>
+      <li>메뉴2</li>
+      <li>메뉴3</li>
+      <li>메뉴4</li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
