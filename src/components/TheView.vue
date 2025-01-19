45f4c1ae441cc5684cefe06a23e5b78445ac3622
seorang42 <seorang42@naver.com>
2025-01-20 00:38:30 +0900

    6-3 Props

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -3,23 +3,16 @@
 		<main>
 			<div class="container text-center py-4">
 				<div class="row g-5">
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
+					<div v-for="post in posts" :key="post.id" class="col col-4">
+						<AppCard
+							:title="post.title"
+							:contents="post.contents"
+							:type="post.type"
+							:is-like="post.isLike"
+							@toggle-like="post.isLike = !post.isLike"
+							:obj="obj"
+							@change-title="obj.title = '김길동'"
+						/>
 					</div>
 				</div>
 			</div>
@@ -28,13 +21,43 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
 import AppCard from './AppCard.vue';
 export default {
 	components: {
 		AppCard,
 	},
 	setup() {
-		return {};
+		const obj = reactive({
+			title: '제목2',
+			contents: '내용2',
+		});
+		const posts = reactive([
+			{ id: 1, title: '제목1', contents: '내용1', isLike: true, type: 'news' },
+			{ id: 2, title: '제목2', contents: '내용2', isLike: true, type: 'news' },
+			{
+				id: 3,
+				title: '제목3',
+				contents: '내용3',
+				isLike: true,
+				type: 'newsasdasf',
+			},
+			{
+				id: 4,
+				title: '제목4',
+				contents: '내용4',
+				isLike: false,
+				type: 'notice',
+			},
+			{
+				id: 5,
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+		return { obj, posts };
 	},
 };
 </script>
