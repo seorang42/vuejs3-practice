b11e093a7fe398caaf1f3185a76bcba711fb4b60
seorang42 <seorang42@naver.com>
2025-01-19 16:38:41 +0900

    4-5 Computed

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
-	<div></div>
+	<div>
+		<h2>{{ teacher.name }}</h2>
+		<h3>강의가 있습니까?</h3>
+		<!-- <p>{{ teacher.lectures.length > 0 ? '있음' : '없음' }}</p> -->
+		<p>{{ hasLecture }}</p>
+		<p>{{ hasLecture }}</p>
+		<p>{{ hasLecture }}</p>
+		<p>{{ existLecture() }}</p>
+		<p>{{ existLecture() }}</p>
+		<p>{{ existLecture() }}</p>
+		<button v-on:click="counter++">Counter {{ counter }}</button>
+		<h2>이름</h2>
+		<p>{{ fullName }}</p>
+	</div>
 </template>
 
 <script>
-import { reactive, readonly } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 export default {
 	setup() {
-		const original = reactive({
-			count: 0,
+		const teacher = reactive({
+			name: '짐코딩',
+			lectures: ['HTML/CSS', 'JavaScript', 'Vue3'],
 		});
-		const copy = readonly(original);
-		original.count++;
-		copy.count++; // 불가능
-		console.log(original.count);
-		console.log(copy.count);
-		return {};
+
+		const hasLecture = computed(() => {
+			console.log('computed');
+			return teacher.lectures.length > 0 ? '있음' : '없음';
+		});
+		const existLecture = () => {
+			console.log('method');
+			return teacher.lectures.length > 0 ? '있음' : '없음';
+		};
+		const counter = ref(0);
+
+		const firstName = ref('홍');
+		const lastName = ref('길동');
+		const fullName = computed({
+			get() {
+				return firstName.value + ' ' + lastName.value;
+			},
+			set(value) {
+				const [first, last] = value.split(' ');
+				firstName.value = first;
+				lastName.value = last;
+			},
+		});
+		console.log(fullName.value);
+		fullName.value = '짐 코딩';
+		console.log(firstName.value, lastName.value);
+		return { teacher, hasLecture, existLecture, counter, fullName };
 	},
 };
 </script>
