9fad120bd5f3fb26e89b943392286c8a3ff0cfff
seorang42 <seorang42@naver.com>
2025-01-19 23:53:05 +0900

    6-1 컴포넌트 기초

---
+++ src/components/TheView.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<main>
+			<div class="container text-center py-4">
+				<div class="row g-5">
+					<div class="col col-4">
+						<AppCard></AppCard>
+					</div>
+					<div class="col col-4">
+						<AppCard></AppCard>
+					</div>
+					<div class="col col-4">
+						<AppCard></AppCard>
+					</div>
+					<div class="col col-4">
+						<AppCard></AppCard>
+					</div>
+					<div class="col col-4">
+						<AppCard></AppCard>
+					</div>
+					<div class="col col-4">
+						<AppCard></AppCard>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import AppCard from './AppCard.vue';
+export default {
+	components: {
+		AppCard,
+	},
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
