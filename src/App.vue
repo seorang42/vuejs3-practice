8281c7704be9312afd1fbd6ea9244934f57f682f
seorang42 <seorang42@naver.com>
2025-01-19 15:45:02 +0900

    4-4 반응형 기초 (Reactivity)

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,21 @@
 <template>
-	<div>
-		<h2>보간법</h2>
-		<p>{{ message }}</p>
-		<p v-once>{{ message }}</p>
-		<button v-on:click="message = message + `!`">click</button>
-		<h2>v-html</h2>
-		<p>{{ rawHtml }}</p>
-		<p v-html="rawHtml"></p>
-		<h2>속성 바인딩</h2>
-		<div v-bind:title="dynamicTitle">마우스를 올려보세요</div>
-		<input type="text" value="홍길동" :disabled="isInputDisabled" />
-		<input type="text" v-bind="attrs" />
-		<h2>Javascript</h2>
-		{{ message.split('').reverse().join('') }}
-		{{ isInputDisabled ? '예' : '아니오' }}
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, readonly } from 'vue';
 
 export default {
 	setup() {
-		const message = ref('안녕하세요');
-		const rawHtml = ref('<strong>안녕하세요</strong>');
-		const dynamicTitle = ref('안녕하세요');
-		const isInputDisabled = ref(true);
-		const attrs = ref({
-			type: 'password',
-			value: '12345678',
-			disabled: false,
+		const original = reactive({
+			count: 0,
 		});
-
-		return {
-			message,
-			rawHtml,
-			dynamicTitle,
-			isInputDisabled,
-			attrs,
-		};
+		const copy = readonly(original);
+		original.count++;
+		copy.count++; // 불가능
+		console.log(original.count);
+		console.log(copy.count);
+		return {};
 	},
 };
 </script>
